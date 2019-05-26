d052faeac51adbb10365a967b4432e81f2c65647
Dmitriy Kozlov <kdmitriys@gmail.com>
2019-05-26 15:15:11 +0300

    правки

--- source/js/main.js
+++ source/js/main.js
@@ -14,4 +14,33 @@ burger.addEventListener('click', function(e) {
   }
 });
 
+// /////////////////modal
+
+// var modal = document.querySelector('.purchase-popup');
+// var openModal = document.querySelector('.js-modal');
+// const mOpened = 'modal--opened';
+// const mClosed = 'modal--closed';
+// console.log(modal);
+
+// var closeModal = function() {
+//   modal.classList.remove(mOpened);
+//   modal.classList.add(mClosed);
+// };
+
+// openModal.addEventListener('click', function(e) {
+//   if(modal.classList.contains(mClosed)) {
+//     modal.classList.remove(mClosed);
+//     modal.classList.add(mOpened);
+//   }
+// });
+
+// window.addEventListener('click', function(e) {
+//   if(modal.classList.contains(mOpened) && (e.target.className.indexOf('purchase-popup') === -1)) {
+//     // closeModal();
+//   }
+//   console.log(e.target);
+//   console.log(e.target.className);
+//   console.log(e.target.className.indexOf('purchase-popup'));
+// });
+
 //End
